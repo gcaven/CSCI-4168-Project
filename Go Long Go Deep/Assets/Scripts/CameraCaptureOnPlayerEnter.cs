d1ec8cd1502a6969f5195a9ad8636ceac2079ae8
Geoffrey Caven <geoffreycaven@gmail.com>
2017-11-05 15:39:38 -0400

    Build a small test level, implement camera snapping and resizing to rooms as player moves between them

---
+++ Go Long Go Deep/Assets/Scripts/CameraCaptureOnPlayerEnter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCaptureOnPlayerEnter : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(gameObject.name + ": enter");
+        if (other.tag == "Player") {
+            CameraMovement.target = gameObject;
+            Vector2 size = GetComponent<BoxCollider2D>().size;
+            float maxSize = Mathf.Max(size.x/2 + 2, size.y/2 + 2);
+            CameraMovement.orthoSizeTarget = maxSize;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        //Debug.Log(gameObject.name + ": exit");
+        //if (other.tag == "Player") {
+            // CameraMovement.target = other.gameObject;
+        //}
+    }
+}
+    
+    
