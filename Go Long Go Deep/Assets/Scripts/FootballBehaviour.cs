0f4648e62028ea7642bd4587ed509495d54de15d
Geoffrey Caven <geoffreycaven@gmail.com>
2017-11-16 14:41:03 -0400

    fix the ball getting caught on the player when thrown downwards, adjust ball spawn position based on velocity of player

--- Go Long Go Deep/Assets/Scripts/FootballBehaviour.cs
+++ Go Long Go Deep/Assets/Scripts/FootballBehaviour.cs
@@ -14,9 +14,28 @@ public class FootballBehaviour : MonoBehaviour {
 	// Use this for initialization
     public void Initialize(Vector2 position, Vector2 velocity) {
         isMoving = true;
-        GetComponent<Rigidbody2D>().position = position;
+		GetComponent<Rigidbody2D>().position = NormalizePositionForDirection(position, velocity);
         setDirection(velocity);
     }
+
+	// Player position if defined at the top left of their bounding box
+	// We have to adjust the ball spawn postion based on movement to avoid getting the player
+	// caught on the ball, and make the spawn point seem natural and correct.
+	// The values chosen are arbitrary based on what look correct in motion
+	Vector2 NormalizePositionForDirection(Vector2 position, Vector2 velocity) {
+		Vector2 finalPosition = new Vector2(position.x, position.y + 0.1f);
+		if (velocity.x > 0) {
+			finalPosition.x += 0.2f;
+		} else if (velocity.x < 0) {
+			finalPosition.x -= 0.2f;
+		}
+		if (velocity.y > 0) {
+			finalPosition.y += 0.2f;
+		} else if (velocity.y < 0) {
+			finalPosition.y -= 1.2f;
+		}
+		return finalPosition;
+	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
