905f3451c0041ab401cbca12825c5d4c99d04257
Geoffrey Caven <geoffreycaven@gmail.com>
2017-11-21 15:47:43 -0400

    first try fading out inactive rooms. They get dim but they sure don't fade though

--- Go Long Go Deep/Assets/Scripts/CameraCaptureOnPlayerEnter.cs
+++ Go Long Go Deep/Assets/Scripts/CameraCaptureOnPlayerEnter.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraCaptureOnPlayerEnter : MonoBehaviour {
-    // Use this for initialization
-    void Start() {
 
+	private float damping = 0.5f;
+
+    void Start() {
+		foreach (Renderer renderer in GetComponentsInChildren<Renderer>()) {
+			renderer.material.color = new Color(
+				renderer.material.color.r,
+				renderer.material.color.g,
+				renderer.material.color.g,
+				0.1f
+			);
+		}
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -16,15 +25,30 @@ public class CameraCaptureOnPlayerEnter : MonoBehaviour {
             float maxSize = Mathf.Max(size.x/2 + 2, size.y/2 + 2);
             CameraMovement.orthoSizeTarget = maxSize;
 			Player.setCurrentRoom (gameObject);
+			transitionAlpha (1f);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        //Debug.Log(gameObject.name + ": exit");
-        //if (other.tag == "Player") {
-            // CameraMovement.target = other.gameObject;
-        //}
+        Debug.Log(gameObject.name + ": exit");
+        if (other.tag == "Player") {
+			transitionAlpha (0.1f);
+        }
     }
+
+	private void transitionAlpha(float targetAlpha) {
+		foreach (Renderer renderer in gameObject.GetComponentsInChildren<Renderer>()) {
+			Material currentMaterial = renderer.material;
+			Material targetMaterial = renderer.material;
+			targetMaterial.color = new Color(
+				currentMaterial.color.r, 
+				currentMaterial.color.g,
+				currentMaterial.color.b,
+				targetAlpha
+			);
+			renderer.material.Lerp(currentMaterial, targetMaterial, Time.deltaTime * damping);
+		}
+	}
 }
     
     
