582fe4859317c0ef888d93604f91052f1a929ffb
Geoffrey Caven <geoffreycaven@gmail.com>
2017-11-24 18:47:06 -0400

    Skeleman animations all figured out. Skeleman attacks when the player is close, and now also correctly targets the center of the player. Has an attack cooldown, which doesn't seem to be working correctly.  Added animator triggers for death and hits, but damage stuff is not implemented yet

--- Go Long Go Deep/Assets/Scripts/SkeletonController.cs
+++ Go Long Go Deep/Assets/Scripts/SkeletonController.cs
@@ -7,12 +7,17 @@ public class SkeletonController : MonoBehaviour {
 	private Player player;
 	private Vector2 initialPos;
 	private static Animator animator;
+	private static Rigidbody2D skeleBody;
 	public float movementSpeed = 100;
+	public float attackMovementCooldown = 1.0f;
+	private float attackMovementTimeout;
 
 	// Use this for initialization
 	void Start () {
 		initialPos = transform.position;
 		animator = GetComponent<Animator>(); 
+		skeleBody = GetComponent<Rigidbody2D> ();
+		attackMovementTimeout = 0f;
 	}
 	
 	// Update is called once per frame
@@ -21,7 +26,12 @@ public class SkeletonController : MonoBehaviour {
 			player = Player.GetPlayer ();
 		}
 		if (gameObject.transform.parent.gameObject == Player.GetCurrentRoom ()) {
-			moveToTargetPosition(player.transform.position);
+			if (attackMovementTimeout <= 0f) {
+				moveToTargetPosition (player.transform.position);
+			} else {
+				attackMovementTimeout -= Time.deltaTime;
+				skeleBody.velocity = Vector2.zero;
+			}
 		} else {
 			moveToTargetPosition(initialPos);
 		}
@@ -29,7 +39,7 @@ public class SkeletonController : MonoBehaviour {
 
 
 	private void setAnimation(Vector2 velocity) {
-		if (System.Math.Round(velocity.x, 1) != 0f || System.Math.Round(velocity.y, 1) != 0f) {
+		if (velocity.x != 0f || velocity.y != 0f) {
 			animator.SetBool("isWalking", true);
 		} else {
 			animator.SetBool("isWalking", false);
@@ -49,16 +59,26 @@ public class SkeletonController : MonoBehaviour {
 
 	void moveToTargetPosition(Vector2 targetPos) {
 		Vector2 positionDiff = new Vector2 (
-			transform.position.x - targetPos.x,
-			transform.position.y - targetPos.y
+			(float)System.Math.Round(transform.position.x - targetPos.x,1),
+			(float)System.Math.Round(transform.position.y - targetPos.y,1)
 		);
+		if (Mathf.Abs(positionDiff.x) <= 1.2f && Mathf.Abs(positionDiff.y) <= 1.2f) {
+			Debug.Log ("attack: " + positionDiff);
+			attackPlayer ();
+			skeleBody.velocity = Vector2.zero;
+			return;
+		}
 		Vector2 velocity = new Vector2 (
-			Mathf.Clamp (1 - positionDiff.x, -1f, 1f) * movementSpeed * Time.deltaTime,
-			Mathf.Clamp (1 - positionDiff.y, -1f, 1f) * movementSpeed * Time.deltaTime
+			Mathf.Clamp (0 - positionDiff.x, -1f, 1f) * movementSpeed * Time.deltaTime,
+			Mathf.Clamp (0 - positionDiff.y, -1f, 1f) * movementSpeed * Time.deltaTime
 		);
-//		Debug.Log (velocity);
-		Rigidbody2D skeleBody = GetComponent<Rigidbody2D> ();
 		setAnimation (velocity);
 		skeleBody.velocity = velocity;
+	}
+
+	void attackPlayer() {
+		animator.SetTrigger("isAttacking");
+		animator.SetBool("isWalking", false);
+		attackMovementTimeout = attackMovementCooldown;
 	}
 }
